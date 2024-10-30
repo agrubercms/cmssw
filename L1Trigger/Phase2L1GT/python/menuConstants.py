d65c9127ba280c2c332cbaef3d8c43ddd7a6e4f9
Andreas Gruber <andreas.gruber@cern.ch>
2024-10-30 15:55:05 +0100

    fix tau L1 thresholds

--- L1Trigger/Phase2L1GT/python/menuConstants.py
+++ L1Trigger/Phase2L1GT/python/menuConstants.py
@@ -2,8 +2,12 @@
 # scalings_fname = "/eos/home-a/alobanov/SWAN_projects/L1T-Ph2-Menu/L1Nano/scalings_v44.yaml"
 # with open(scalings_fname, "r") as f:
 #     scalings = yaml.safe_load(f)
+import os
 import json
-scalings_fname = "./scalings_v44.json"
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+scalings_fname = os.path.join(script_dir, 'scalings_v44.json')
+
 with open(scalings_fname, 'r') as file:
     scalings = json.load(file)
 
