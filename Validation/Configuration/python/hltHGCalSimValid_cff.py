b3c0782351c2b02ebd6e57429572f39e6654ff2f
Andreas Gruber <andreas.gruber@cern.ch>
2025-12-04 16:50:19 +0100

    add ticlTauValidation module

--- Validation/Configuration/python/hltHGCalSimValid_cff.py
+++ Validation/Configuration/python/hltHGCalSimValid_cff.py
@@ -8,6 +8,7 @@ from SimCalorimetry.HGCalAssociatorProducers.LCToCPAssociation_cfi import layerC
 from SimCalorimetry.HGCalAssociatorProducers.SimClusterToCaloParticleAssociation_cfi import SimClusterToCaloParticleAssociation
 from SimCalorimetry.HGCalAssociatorProducers.TSToSimTSAssociation_cfi import  allTrackstersToSimTrackstersAssociationsByLCs as _allTrackstersToSimTrackstersAssociationsByLCs
 from SimCalorimetry.HGCalAssociatorProducers.hitToSimClusterCaloParticleAssociator_cfi import hitToSimClusterCaloParticleAssociator as _hitToSimClusterCaloParticleAssociator
+from SimCalorimetry.HGCalAssociatorProducers.SimTauProducer_cfi import *
 
 from Validation.HGCalValidation.HLT_TICLIterLabels_cff import hltTiclIterLabels as _hltTiclIterLabels
 
@@ -84,7 +85,8 @@ hltHgcalAssociatorsTask = cms.Task(hltHGCalRecHitMapProducer,
                                    hltAllTrackstersToSimTrackstersAssociationsByLCs,
                                    hltAllHitToTracksterAssociations,
                                    hltHitToSimClusterCaloParticleAssociator,
-                                   hltAllTrackstersToSimTrackstersAssociationsByHits
+                                   hltAllTrackstersToSimTrackstersAssociationsByHits,
+                                   SimTauProducer
                                    )
 
 hltHgcalPrevalidation = cms.Sequence(
