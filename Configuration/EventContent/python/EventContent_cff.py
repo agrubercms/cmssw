45eb65b521337cfa70fb0eeac63a05113a2c963f
gourab saha <gourab.saha@cern.ch>
2026-06-19 22:30:14 +0200

    keep _hltParticleFlowTmp for pfcand collection to be used in ticlTauValidator

--- Configuration/EventContent/python/EventContent_cff.py
+++ Configuration/EventContent/python/EventContent_cff.py
@@ -736,6 +736,12 @@ phase2_common.toModify(FEVTDEBUGHLTEventContent,
                            'keep *_hltMergeLayerClusters_*_*',
                            'keep *_hltParticleFlowRecHit*_*_*',
                            'keep *_hltEgammaGsfTracksL1Seeded_*_*',
+                           'keep *_hltEgammaGsfTracksUnseeded_*_*',
+                           'keep *_hltPFMET_*_*',
+                           'keep *_hltPFPuppiMET_*_*',
+                           'keep *_hltPFPuppiMETTypeOne_*_*',
+                           'keep *_hltHpsPFTauDeepTauProducer_*_*',
+                           'keep *_hltParticleFlowTmp*_*_*'
                        ])
 
 phase2_muon.toModify(FEVTDEBUGHLTEventContent, 
